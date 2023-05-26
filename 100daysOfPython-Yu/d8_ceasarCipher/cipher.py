8f2408d425ab5b8df84aa643ad4b49e25b82a246
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2023-05-27 00:02:04 +0200

    got rid of OOR bug

--- 100daysOfPython-Yu/d8_ceasarCipher/cipher.py
+++ 100daysOfPython-Yu/d8_ceasarCipher/cipher.py
@@ -5,6 +5,13 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+ #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
 def encrypt(text,shift):
   text_list = list(text)
   cipher_array = []
@@ -12,20 +19,19 @@ def encrypt(text,shift):
   
   for x in range(0,len(text_list)):
     index = alphabet.index(text_list[x])
-    
-    y = alphabet[index + shift]
+
+    if (index + shift) >= len(alphabet):
+      new_shift = (index + shift) - len(alphabet)
+      y = alphabet[new_shift]
+      cipher_array.append(y)
+    else:
+      y = alphabet[index + shift]
     
     cipher_array.append(y)
       
   cipher_text = "".join(cipher_array)
   print(f"The encoded text is {cipher_text}")
   
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
 
     ##HINT: How do you get the index of an item in a list:
     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
