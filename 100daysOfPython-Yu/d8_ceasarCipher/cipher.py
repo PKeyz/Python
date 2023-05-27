cad9802e52bee0df5379836fb1695e5f0a198107
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2023-05-27 13:34:58 +0200

    adding decrypt functionality

--- 100daysOfPython-Yu/d8_ceasarCipher/cipher.py
+++ 100daysOfPython-Yu/d8_ceasarCipher/cipher.py
@@ -33,11 +33,30 @@ def encrypt(text,shift):
   print(f"The encoded text is {cipher_text}")
   
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+def decrypt(text, shift):
+  text_list = list(text)
+  cipher_array = []
+  cipher_text = ""
+  
+  for x in text_list:
+    index = alphabet.index(x)
+    y = alphabet[index - shift]
+    
+    cipher_array += y
+      
+  cipher_text = "".join(cipher_array)
+  print(f"The encoded text is {cipher_text}")
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-
-encrypt(text,shift)
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+  encrypt(text, shift)
+else:
+  decrypt(text, shift)
