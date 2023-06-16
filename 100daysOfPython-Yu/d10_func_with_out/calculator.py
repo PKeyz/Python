233929756b29d700bb00c4383488ad863503fe9d
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2023-06-16 22:48:39 +0200

    inserted docstrings for definitions

--- 100daysOfPython-Yu/d10_func_with_out/calculator.py
+++ 100daysOfPython-Yu/d10_func_with_out/calculator.py
@@ -26,22 +26,37 @@ What's the next number?: INPUT
 Type 'y' to continue calculating with 15.0, or type 'n' to start a new calculation: INPUT
 """
 def addition(a, b):
+    """
+    Taking two integers as inputs, adds them and returns a result value
+    """
     result = a + b
     return result
 
 def subtraction(a, b):
+    """
+    Taking two integers as inputs, subtracts them and returns a result value
+    """
     result = a - b
     return result
 
 def division(a, b):
+    """
+    Taking two integers as inputs, divides them and returns a result value
+    """
     result = a / b
     return result
 
 def multiplication(a, b):
+    """
+    Taking two integers as inputs, multiplies them and returns a result value
+    """
     result = a * b
     return result
 
 def ask_for_continuation(ask_to_continue):
+    """
+    Asks user input as string, if input is "y", returns is_continuing True, else False
+    """
     if ask_to_continue == "y":
         is_continuing = True
         return is_continuing
@@ -49,18 +64,36 @@ def ask_for_continuation(ask_to_continue):
         is_continuing = False
         return is_continuing
     
+def calculate(a, b, operation):
+    """
+    Taking two integers and the operation string as inputs to calculate the result of the operation
+    """
+    if operation == "+":
+        addition(a, b)
+    elif operation == "-":
+        subtraction(a, b)
+    elif operation == "/":
+        division(a, b)
+    elif operation == "*":
+        multiplication(a, b)
+    else:
+        print("Wrong operation selected, Try again\n")
 
 is_continuing = True
 
 while(is_continuing):
     
+    a = input("What's the first number?: ")
+    print("+\n-\n/\n*\n")
+    operation = input("Pick an operation: ")
+    b = input("What's the next number?: ")
+
+    calculate(a, b, operation)
+
+
+
+    print(f"")
+
     ask_to_continue = input("Type 'y' to continue calculating with 15.0, or type 'n' to start a new calculation: ")
     ask_for_continuation(ask_to_continue)
     
-
-
-a = input("What's the first number?: ")
-print("+\n-\n/\n*\n")
-operation = input("Pick an operation: ")
-b = input("What's the next number?: ")
-print(f"")
