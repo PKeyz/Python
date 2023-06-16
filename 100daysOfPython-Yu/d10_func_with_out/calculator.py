69347fb725fd68b0c1899f3c54eb413c20563ffc
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2023-06-16 22:33:13 +0200

    0_skeleton

--- 100daysOfPython-Yu/d10_func_with_out/calculator.py
+++ 100daysOfPython-Yu/d10_func_with_out/calculator.py
@@ -25,3 +25,41 @@ What's the next number?: INPUT
 3.0 * 5.0 = 15.0
 Type 'y' to continue calculating with 15.0, or type 'n' to start a new calculation: INPUT
 """
+def addition(a, b):
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+def division(a, b):
+    result = a / b
+    return result
+
+def multiplication(a, b):
+    result = a * b
+    return result
+
+def ask_for_continuation(is_continuing):
+    if is_continuing == "y":
+        return True
+    else:
+        return False
+    
+
+is_continuing = True
+
+
+while(is_continuing):
+    
+    ask_for_continuation()
+    
+
+
+a = input("What's the first number?: ")
+print("+\n-\n/\n*\n")
+operation = input("Pick an operation: ")
+b = input("What's the next number?: ")
+print(f"")
+is_continuing = input("Type 'y' to continue calculating with 15.0, or type 'n' to start a new calculation: ")
