5bdc1948c6b840588d5ac1fadefd4deb86e5e858
philipp.keyzman <real.phil94@gmail.com>
2023-06-18 22:56:28 +0200

    inserted the operations

---
+++ 100daysOfPython-Yu/d10_func_with_out/calculator_yu.py
@@ -0,0 +1,19 @@
+"""
+This version is following Yu's instructions, so the result will look different
+"""
+
+#Add
+def addition(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtraction(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiplication(n1, n2):
+    return n1 * n2
+#Divide
+def division(n1, n2):
+    return n1 / n2
+
