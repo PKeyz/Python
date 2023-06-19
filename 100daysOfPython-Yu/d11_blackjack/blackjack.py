25759a3229bf537b48f01ff7130c0b038eeb5aaf
philipp.keyzman <real.phil94@gmail.com>
2023-06-19 20:18:35 +0200

    added my first ideas

---
+++ 100daysOfPython-Yu/d11_blackjack/blackjack.py
@@ -0,0 +1,36 @@
+"""
+First ideas:
+
+Rules:
+deck of cards -> shuffle
+give card to player + card to dealer
+10,K,Q = value 10
+numbers = number_value
+
+def check_sum(card_value):
+    if previous_answer + card_value <= 21:
+        print("Player wins!")
+
+    else:
+        print("Dealer wins!")
+
+if hit_button:
+    get new card from deck
+    count new check_sum
+
+
+prints:
+
+Your Cards[9,10]
+Computer's first card: 8
+Type 'y' to get another card, type 'n' to pass: 
+Your  final hand: [9,10]
+Computer's final hand: [8,10]
+You Win
+Do you want to play a game of blackjack? Type 'y' or 'n': 
+
+in python do while done with do block - and recursion inside loop
+"""
+
+
+
