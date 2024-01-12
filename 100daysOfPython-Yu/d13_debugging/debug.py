be9e8486da1c1682c52c2fd578e07145ef920cc8
Philipp Keyzman <real.phil94@gmail.com>
2024-01-12 13:36:46 +0100

    last debug

--- 100daysOfPython-Yu/d13_debugging/debug.py
+++ 100daysOfPython-Yu/d13_debugging/debug.py
@@ -64,3 +64,15 @@
 #         print("Leap year")
 # else:
 #     print("Not a leap year")
+
+target = int(input())
+
+for number in range(1,target + 1):
+    if (number % 3 == 0) and (number % 5 == 0):
+        print("FizzBuzz")
+    elif (number % 3 == 0):
+        print("Fizz")
+    elif (number % 5 == 0):
+        print("Buzz")
+    else:
+        print({number})
