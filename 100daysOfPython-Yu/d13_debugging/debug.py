f1fb5af6abde0d82abc26ed75022a07048dbc4ef
Philipp Keyzman <real.phil94@gmail.com>
2024-01-12 13:21:52 +0100

    leap year bug

--- 100daysOfPython-Yu/d13_debugging/debug.py
+++ 100daysOfPython-Yu/d13_debugging/debug.py
@@ -50,3 +50,17 @@
 #     print("This is an even number.")
 # else:
 #     print("This is an odd number.")
+
+# #Which year do you want to check?
+# year = int(input())
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year")
+#         else:
+#             print("Not a leap year")
+#     else:
+#         print("Leap year")
+# else:
+#     print("Not a leap year")
