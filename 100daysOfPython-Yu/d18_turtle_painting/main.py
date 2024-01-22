be3236713e8783261807dc5a25cd8e66255c9014
Philipp Keyzman <real.phil94@gmail.com>
2024-01-22 15:21:27 +0100

    draw multiple shapes with random color

--- 100daysOfPython-Yu/d18_turtle_painting/main.py
+++ 100daysOfPython-Yu/d18_turtle_painting/main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 phil_turtle = turtle.Turtle()
 phil_turtle.shape('classic')
@@ -15,6 +16,52 @@ phil_turtle.color('red2')
 #     phil_turtle.penup()
 #     phil_turtle.forward(10)
 #     phil_turtle.pendown()
+
+# Draw triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, decagon in sequence
+# from the same starting position
+# changing to a random color for each row
+
+counter = 0
+distance = 100
+shapes = [
+    {'shape': 'triangle',
+     'sides': 3,
+     'angles': 120},
+    {'shape': 'square',
+     'sides': 4,
+     'angles': 90},
+    {'shape': 'pentagon',
+     'sides': 5,
+     'angles': 72},
+    {'shape': 'hexagon',
+     'sides': 6,
+     'angles': 60},
+    {'shape': 'heptagon',
+     'sides': 7,
+     'angles': 51 + 3/7},
+    {'shape': 'octagon',
+     'sides': 8,
+     'angles': 45},
+    {'shape': 'nonagon',
+     'sides': 9,
+     'angles': 40},
+    {'shape': 'decagon',
+     'sides': 10,
+     'angles': 36}
+]
+color = f'#{random.randrange(256**3):06x}'
+
+while counter < 8:
+    color = f'#{random.randrange(256 ** 3):06x}'
+    phil_turtle.color(color)
+
+    for y in range(shapes[counter].get('sides')):
+        phil_turtle.forward(distance)
+        phil_turtle.right(shapes[counter].get('angles'))
+    counter += 1
+
+
+
 
 # must be at the end of the code to visualize, else returns empty screen
 screen = turtle.Screen()
