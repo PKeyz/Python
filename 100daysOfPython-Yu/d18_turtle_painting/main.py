c154d04f40f810c0b2aaa840989cfcedafd90a46
Philipp Keyzman <real.phil94@gmail.com>
2024-01-22 14:22:03 +0100

    draw a dashed line

--- 100daysOfPython-Yu/d18_turtle_painting/main.py
+++ 100daysOfPython-Yu/d18_turtle_painting/main.py
@@ -4,11 +4,17 @@ phil_turtle = turtle.Turtle()
 phil_turtle.shape('classic')
 phil_turtle.color('red2')
 
-# turtle square
-for x in range(4):
-    phil_turtle.forward(100)
-    phil_turtle.right(90)
+# # turtle square
+# for x in range(4):
+#     phil_turtle.forward(100)
+#     phil_turtle.right(90)
 
+# # draw dashed line: line 10 gap 10 repeat 50 times
+# for x in range(15):
+#     phil_turtle.forward(10)
+#     phil_turtle.penup()
+#     phil_turtle.forward(10)
+#     phil_turtle.pendown()
 
 # must be at the end of the code to visualize, else returns empty screen
 screen = turtle.Screen()
