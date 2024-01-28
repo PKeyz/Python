107594d8eaa11a56eb203cef447910bdead5dfb5
Philipp Keyzman <real.phil94@gmail.com>
2024-01-28 20:39:41 +0100

    added random_coordinates, create_food

--- 100daysOfPython-Yu/d20_snake_game/functions.py
+++ 100daysOfPython-Yu/d20_snake_game/functions.py
@@ -1,18 +1,56 @@
 import turtle
+import random
 
 
 # TODO 1. create snake body
 # a)define 1 unit of body
 # b)start game with 3 units
+
+def create_one_snake(x: int, y: int, color: str):
+    snake = turtle.Turtle('square')
+    snake.color('black')
+    snake.penup()
+    #snake.shapesize(0.5, 0.5)
+    snake.setposition(x, y)
+    snake.color(color)
+
+
 # TODO 2. move the snake
 def move_left():
     turtle.left(90)
+
+
 def move_right():
     turtle.right(90)
+
+
 # TODO 3. create snake food
 # TODO 4. detect collision with food
 # TODO 5. create a scoreboard
 # TODO 6. detect collision with wall
 # TODO 7. detect collision with tail
 
-import random
+
+def define_rand_coordinate():
+    """
+    defines a random position on the 600*600 screen
+    """
+    coordinate = (random.randint(-300, 300), random.randint(-300, 300))
+    return coordinate
+
+# def new_food_pos():
+#     new_position = ()
+#     is_used = True
+#     while is_used:
+#         temp_coordinate = define_rand_coordinate()
+#         if temp_coordinate !=  coordinates of snake turtles
+#             new_position = temp_coordinate
+#             is_used = False
+#             return new_position
+#         else:
+#             continue
+
+def create_food(new_food_pos):
+    x = new_food_pos()[0]
+    y = new_food_pos()[1]
+    create_one_snake(x, y, 'blue')
--- 100daysOfPython-Yu/d20_snake_game/main.py
+++ 100daysOfPython-Yu/d20_snake_game/main.py
@@ -5,17 +5,27 @@ import functions
 
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('black')
+screen.bgcolor('white')
 screen.title('My Snake Game')
 
-snake = turtle.Turtle('square')
-snake.color('white')
-snake.penup()
-snake.shapesize(1, 3)
+snake_lst = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+# snake_lst.append(functions.create_one_snake(0, 0, 'red'))
+# snake_lst.append(functions.create_one_snake(-20, 0, 'red'))
+# snake_lst.append(functions.create_one_snake(-40, 0, 'red'))
+functions.create_one_snake(-50, 70, 'blue')
 
-is_running = True
-while is_running:
-    snake.forward(1)
+i = 0
+j = 0
+for x in range(3):
+    snake_lst.append(functions.create_one_snake(starting_positions[i][j], starting_positions[i][j], 'red'))
+    i += 1
+    j += 1
+
+# is_running = True
+# while is_running:
+#     for snake in snake_lst:
+#         snake.forward(1)
 
 
 
