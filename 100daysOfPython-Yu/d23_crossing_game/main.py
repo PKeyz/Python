d979ee9c2863c51b075fc9374bb88b0af82a8b45
Philipp Keyzman <real.phil94@gmail.com>
2024-04-20 17:33:08 +0200

    init turtle

--- 100daysOfPython-Yu/d23_crossing_game/main.py
+++ 100daysOfPython-Yu/d23_crossing_game/main.py
@@ -1,14 +1,17 @@
 import time
 from turtle import Screen
-from player import Player
+import player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor('black')
 screen.tracer(0)
+player = player.Player()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
--- 100daysOfPython-Yu/d23_crossing_game/player.py
+++ 100daysOfPython-Yu/d23_crossing_game/player.py
@@ -1,7 +1,20 @@
+import turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+SPEED = 'fastest'
 
 
-class Player:
-    pass
+class Player(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.shape('turtle')
+        self.shapesize(1)
+        self.color('green')
+        self.speed(SPEED)
+        self.setheading(90)
+        self.score = 0
+
