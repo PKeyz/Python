b15cbbe7b4ea0dc2cb52f4178d89f67f147487bd
Philipp Keyzman <real.phil94@gmail.com>
2024-04-20 17:52:11 +0200

    car init

--- 100daysOfPython-Yu/d23_crossing_game/car_manager.py
+++ 100daysOfPython-Yu/d23_crossing_game/car_manager.py
@@ -1,7 +1,19 @@
+import turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shapesize(1, 2)
+        self.penup()
+        self.color(COLORS[random.randint(0, 5)])
+        self.speed(STARTING_MOVE_DISTANCE)
+        self.goto(280, random.randint(-280, 280))
+        self.setheading(180)
+        self.shape('square')
+
--- 100daysOfPython-Yu/d23_crossing_game/main.py
+++ 100daysOfPython-Yu/d23_crossing_game/main.py
@@ -1,7 +1,7 @@
 import time
 from turtle import Screen
 import player
-from car_manager import CarManager
+import car_manager
 from scoreboard import Scoreboard
 
 screen = Screen()
@@ -11,13 +11,17 @@ screen.tracer(0)
 player = player.Player()
 
 screen.listen()
-
+#player control buttons
 screen.onkeypress(player.move_up, "w")
 screen.onkeypress(player.move_up, "W")
 screen.onkeypress(player.move_up, "Up")
 
+car = car_manager.CarManager()
+#game
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+
+screen.exitonclick()
