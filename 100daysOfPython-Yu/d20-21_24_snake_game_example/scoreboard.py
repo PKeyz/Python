8e8cd5dec3ec9a20755204fcf7830da77d6636a7
Philipp Keyzman <real.phil94@gmail.com>
2024-04-29 19:38:43 +0200

    updated high_score saving

--- 100daysOfPython-Yu/d20-21_24_snake_game_example/scoreboard.py
+++ 100daysOfPython-Yu/d20-21_24_snake_game_example/scoreboard.py
@@ -4,16 +4,24 @@ MOVEMENT = False
 ALIGNMENT = "center"
 FONT = ("Courier", 15, "bold")
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.high_score = self.read_highscore()
+        self.data_highscore = None
         self.score = 0
         self.hideturtle()
         self.penup()
         self.color('white')
         self.goto(0, 270)
-        self.high_score = 0
+
+    def read_highscore(self):
+        with open('data.txt', 'r') as data_file:
+            self.data_highscore = data_file.read()
+        self.high_score = int(self.data_highscore)
+        return self.high_score
 
     def update_scoreboard(self):
         self.clear()
@@ -26,5 +34,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", 'w+') as data_file:
+                data_file.write(f'{self.high_score}')
         self.score = 0
         self.update_scoreboard()
