b5d5a3f46a712b2ec179a17b59d17f6a9feac1b1
Philipp Keyzman <real.phil94@gmail.com>
2024-05-16 14:37:11 +0200

    refactored csv data to new dict

--- 100daysOfPython-Yu/d25_us_states_game/pop_up.py
+++ 100daysOfPython-Yu/d25_us_states_game/pop_up.py
@@ -8,13 +8,26 @@ class PopUp:
         super().__init__()
         self.show_window()
 
-    def read_csv(self):
+    def read_csv_to_dict(self):
+        """reading the csv file and converting it to a readeable format; returning a dict/list #find out what is better"""
         """reading the csv file and converting it to a readeable format; returning a dict/list #find out what is better"""
         states_data = pandas.read_csv('50_states.csv')
-        #read data into 3 columns State, x, y
-        column_state = states_data['state']
-        column_x = states_data['x']
-        column_y = states_data['y']
+
+        states_data_dict = states_data.to_dict()
+        # print(states_data_dict)
+
+        # Refactored dictionary
+        refactored_state_data = {}
+
+        # Iterate over the 'state' column and construct the refactored dictionary
+        for i in range(len(states_data_dict['state'])):
+            state_name = states_data_dict['state'][i]  # Get the state name
+            x_value = states_data_dict['x'][i]  # Get the corresponding x value
+            y_value = states_data_dict['y'][i]  # Get the corresponding y value
+            refactored_state_data[state_name] = {'x': x_value, 'y': y_value}
+
+        # Print the refactored dictionary
+        print(refactored_state_data)
 
 
     def check_correctness(self):
