be3c9d7bf6b6d63fd75157fbd0f478f12a21675c
Philipp Keyzman <real.phil94@gmail.com>
2024-05-16 15:09:06 +0200

    first state counter in pop-up

--- 100daysOfPython-Yu/d25_us_states_game/pop_up.py
+++ 100daysOfPython-Yu/d25_us_states_game/pop_up.py
@@ -6,11 +6,13 @@ screen = turtle.Screen()
 class PopUp:
     def __init__(self):
         super().__init__()
+        self.correct_answers = 0
+        self.correct_answers_list = []
         self.show_window()
 
     def read_csv_to_dict(self):
-        """reading the csv file and converting it to a readeable format; returning a dict/list #find out what is better"""
-        """reading the csv file and converting it to a readeable format; returning a dict/list #find out what is better"""
+
+        """reading the csv file and converting it to a readeable format; returning a dict with states==key: x and y == value"""
         states_data = pandas.read_csv('50_states.csv')
 
         states_data_dict = states_data.to_dict()
@@ -27,20 +29,24 @@ class PopUp:
             refactored_state_data[state_name] = {'x': x_value, 'y': y_value}
 
         # Print the refactored dictionary
-        print(refactored_state_data)
+        return refactored_state_data
 
-
-    def check_correctness(self):
-        """check if user input fits the states in the states dict/list"""
-    def count_correct(self):
+    def count_correct(self, state: str):
         """counting correct answers and returning a single int"""
+        state = state
+        self.correct_answers_list.append(state)
+        self.correct_answers += 1
+
     def show_window(self):
         """create a pop-up window in the screen to input answers of the user"""
-        #input var for correct states from csv|| dict|| list
-        answer = screen.textinput(f"2/50 States correct!", "What's another state name?")
-        if answer is None or answer.lower().startswith('n'):
-            print("Goodbye!")
-            screen.clear()
-            screen.bye()
+        #input var for correct states from dict
+        answer = screen.textinput(f"{self.correct_answers}/50 States correct!", "What's another state name?")
+
+        #capitalize doen't work!!!!
+        answer.capitalize()
+
+        if answer in (self.read_csv_to_dict()):
+            self.count_correct(answer)
+
         else:
             print("Start!")
