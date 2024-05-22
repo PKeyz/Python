1d290f4d71ec8295493e7a02040386062276a0cf
Philipp Keyzman <real.phil94@gmail.com>
2024-05-22 06:44:50 +0200

    updated pop_up and main to recognize the first input

--- 100daysOfPython-Yu/d25_us_states_game/pop_up.py
+++ 100daysOfPython-Yu/d25_us_states_game/pop_up.py
@@ -3,25 +3,24 @@ import pandas
 
 screen = turtle.Screen()
 
-text = turtle.Turtle
+text = turtle.Turtle()
+
 
 class PopUp:
     def __init__(self):
         super().__init__()
-        self.correct_answers = 0
         self.retry = 0
+        self.correct_answers = 0
+        self.state_data = self.read_csv_to_dict()
         self.correct_answers_list = []
-        self.show_window()
-
-
 
     def read_csv_to_dict(self):
 
-        """reading the csv file and converting it to a readeable format; returning a dict with states==key: x and y == value"""
+        """reading the csv file and converting it to a readeable format; returning a dict with states==key: x and y
+        == value"""
         states_data = pandas.read_csv('50_states.csv')
 
         states_data_dict = states_data.to_dict()
-        # print(states_data_dict)
 
         # Refactored dictionary
         refactored_state_data = {}
@@ -37,32 +36,32 @@ class PopUp:
         return refactored_state_data
 
     def count_correct(self, state: str):
-        """counting correct answers and returning a single int"""
+        """counting correct answers and returning a single integer"""
         state = state
         self.correct_answers_list.append(state)
         self.correct_answers += 1
 
-    def show_window(self):
-        """create a pop-up window in the screen to input answers of the user"""
-        #input var for correct states from dict
-        answer = screen.textinput(f"{self.correct_answers}/50 States correct!", "What's another state name?").capitalize()
+    # def show_window(self):
+    #     """create a pop-up window in the screen to input answers of the user"""
+    #     #input var for correct states from dict
+    #     answer = screen.textinput(f"{self.correct_answers}/50 States correct!", "What's another state name?").title()
+    #
+    #     return answer
 
-        if answer in (self.read_csv_to_dict()):
+    def evaluate_answer(self):
+        answer = screen.textinput(f"{self.correct_answers}/50 States correct!", "What's another state name?").title()
+        if answer in self.read_csv_to_dict():
             self.count_correct(answer)
             return answer
 
         else:
             print("State not in the US! Try again!")
-            self.retry += 1
-            if self.retry == 3:
-                print('To many mistakes, game terminated!')
-
+            # self.retry += 1
+            # if self.retry == 3:
+            #     print('To many mistakes, game terminated!')
 
     def return_coordinates(self, state: str):
         """returns coordinates of the state, if key available in read_csv_to_dict()"""
         state_dict = self.read_csv_to_dict()
         coordinates = state_dict[state]
         return coordinates
-
-
-
