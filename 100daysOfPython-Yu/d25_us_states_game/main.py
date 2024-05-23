84bdfd20263ec090584fbf86f985151b03de3ffd
Philipp Keyzman <real.phil94@gmail.com>
2024-05-23 15:30:32 +0200

    cleaned up file

---
+++ 100daysOfPython-Yu/d25_us_states_game/main.py
@@ -0,0 +1,26 @@
+import turtle
+import pop_up
+
+screen = turtle.Screen()
+
+screen.setup(width=730, height=495)
+screen.bgpic('blank_states_img.gif')
+screen.tracer(0)
+
+pop_up = pop_up.PopUp()
+
+is_game_on = True
+while is_game_on:
+    user_input = pop_up.user_input()
+
+    is_state_correct = pop_up.evaluate_answer(user_input)
+
+    if is_state_correct:
+        coordinates = pop_up.return_coordinates(user_input)
+        print(pop_up.print_coordinates(coordinates))
+    else:
+        print('Try again!')
+
+    pop_up.terminate_game()
+
+screen.mainloop()
