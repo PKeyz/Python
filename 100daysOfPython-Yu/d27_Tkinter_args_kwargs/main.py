5bf44646d72b392b0031e92cadf6ce2d2c97475c
Philipp Keyzman <real.phil94@gmail.com>
2024-06-03 18:04:38 +0200

    tkinter init

---
+++ 100daysOfPython-Yu/d27_Tkinter_args_kwargs/main.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+# Label
+my_label = tk.Label(text="This is a label", font=("Arial", 24, "italic"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
